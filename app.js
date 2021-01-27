8224149bfa0450219612a72f89be66372dc7ec5a
Emily Patsko <Emily.Patsko@softwire.com>
2021-01-27 16:42:29 +0000

    Added CSS styling

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const testController = require('./controllers/testController');
 var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 app.get('/postcode/:postcode', testController.getBusData);
 
 app.listen(port, () => {
 	console.log(`app is listening to port ${port}`);
-	console.log(`directory is ${__dirname}`);
 });
